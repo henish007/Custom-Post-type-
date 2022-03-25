6902908b0512b486cf64727818a32102dd9044f6
henish007 <102350556+henish007@users.noreply.github.com>
2022-03-25 14:57:17 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Recommended way to include parent theme styles.
+ * (Please see http://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme)
+ *
+ */  
+
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_style' );
+				function hello_elementor_child_style() {
+					wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+					wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
+				}
+
+/**
+ * Your code goes below.
+ */
+
+/* Custom Post type Movie */
+
+function custom_post_type() {
+ 
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'Movies', 'Post Type General Name', 'twentythirteen' ),
+        'singular_name'       => _x( 'Movie', 'Post Type Singular Name', 'twentythirteen' ),
+        'menu_name'           => __( 'Movies', 'twentythirteen' ),
+        'parent_item_colon'   => __( 'Parent Movie', 'twentythirteen' ),
+        'all_items'           => __( 'All Movies', 'twentythirteen' ),
+        'view_item'           => __( 'View Movie', 'twentythirteen' ),
+        'add_new_item'        => __( 'Add New Movie', 'twentythirteen' ),
+        'add_new'             => __( 'Add New', 'twentythirteen' ),
+        'edit_item'           => __( 'Edit Movie', 'twentythirteen' ),
+        'update_item'         => __( 'Update Movie', 'twentythirteen' ),
+        'search_items'        => __( 'Search Movie', 'twentythirteen' ),
+        'not_found'           => __( 'Not Found', 'twentythirteen' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+    );
+     
+// Set other options for Custom Post Type
+     
+    $args = array(
+        'label'               => __( 'movies', 'twentythirteen' ),
+        'description'         => __( 'Movie news and reviews', 'twentythirteen' ),
+        'labels'              => $labels,
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'page',
+        'show_in_rest'        => true,
+         
+        // This is where we add taxonomies to our CPT
+        'taxonomies'          => array( 'category' ),
+    );
+     
+    // Registering your Custom Post Type
+    register_post_type( 'movies', $args );
+ 
+}
+ 
+/* Hook into the 'init' action so that the function
+* Containing our post type registration is not 
+* unnecessarily executed. 
+*/
+ 
+add_action( 'init', 'custom_post_type', 0 );
+
+
+
+
+
+
+function text(){
+
+$wcatTerms = get_terms(
+'category', array('hide_empty' => 0, 'number' => 3, 'order' =>'asc', 'parent' =>0));
+        foreach($wcatTerms as $wcatTerm) : 
+    ?>
+            <small><a href="<?php echo get_term_link( $wcatTerm->slug, $wcatTerm->taxonomy ); ?>"><?php echo $wcatTerm->name; ?></a></small>
+                <?php
+                    $args = array(
+                        'post_type' => 'movies',
+                        'order' => 'ASC',
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'category',
+                                'field' => 'slug',
+                                'terms' => $wcatTerm->slug,
+                            )
+                        ),
+                        'posts_per_page' => 1
+                    );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post();
+                ?>
+              <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+              </a>
+            <?php endwhile; wp_reset_postdata(); ?> 
+     <?php endforeach;  
+}
+add_shortcode('test','text');
+
+
+function wpdocs_theme_slug_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'tetetet', 'textdomain' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
+        'before_widget' => '<li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li>',
+        'before_title'  => '<h2 class="widgettitle">',
+        'after_title'   => '</h2>',
+    ) );
+}
+add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
